892e01790e2988b42d2845cc969139e65f8773c6
Charles Sharp <25charlessharp@usd266.com>
2023-02-17 11:58:12 -0600

    done, just need to test

--- Main.java
+++ Main.java
@@ -1,6 +1,45 @@
 public class Main {
+
+  public static String formGradeString(int[] array){
+    String letterGrade = "";
+    for(int num : array){
+      if(array[num] >= 90){
+        letterGrade += "A";
+      }
+      else if(array[num] >= 80){
+        letterGrade += "B";
+      }
+      else if(array[num] >= 70){
+        letterGrade += "C";
+      }
+      else if(array[num] >= 60){
+        letterGrade += "D";
+      }
+      else{
+        letterGrade += "F";
+      }
+
+    }
+    return letterGrade;
+
+  }
+
+  public static String valedictorianName(String[] names, double[] grades){
+    double highest = 0.0;
+    int index = 0;
+    int index2 = 0;
+    for(index = 0; index < grades.length; index ++){
+      if(grades[index] > highest){
+        highest = grades[index];
+        index2 = index;
+      }
+    }
+    return names[index2];
+
+  }
   
   public static void main(String[] args) {
+    
     
   }
 }
